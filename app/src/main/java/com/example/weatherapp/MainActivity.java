69a5edc01f6e491418f7b355e746fc27932f1899
Karol Kostyra <karol.m.kostyra@gmail.com>
2019-05-26 13:39:05 +0200

    v5

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.weatherapp;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.view.View;
@@ -14,16 +21,44 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 //http://api.openweathermap.org/data/2.5/weather?q=Katowice&lang=PL&units=metric&appid=a71fa7369a61aae850d392b6c8d96807
 
 
+
+//tutaj
+import android.location.LocationListener;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.widget.Button;
+//tutaj
+
+
 public class MainActivity extends AppCompatActivity {
+
+    //tutaj
+    private Button saveButton, loadButton, locButton;
+    //private TextView locTextView;
+    private LocationManager locManager;
+    private LocationListener locListener;
+    public static double lat, lon;
+    private TextView getCityName;
+    public String current_city;
+    public String save_city_name = "Warszawa";
+    private boolean switcher;
+    //tutaj
 
     TextView selectCity, cityField, detailsField,
             currentTemperatureField, minTempField, maxTempField,
@@ -43,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
+
         loader = (ProgressBar) findViewById(R.id.loader);
         selectCity = (TextView) findViewById(R.id.select_city);
         cityField = (TextView) findViewById(R.id.city_field);
@@ -58,7 +94,88 @@ public class MainActivity extends AppCompatActivity {
         weatherFont = Typeface.createFromAsset(getAssets(), "fonts/weathericons-regular-webfont.ttf");
         weatherIcon.setTypeface(weatherFont);
 
-        taskLoadUp(city);
+
+        //tutaj
+        final Geocoder geocoder;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        locButton = (Button) findViewById(R.id.loc_button);
+        saveButton = (Button) findViewById(R.id.save_button);
+        loadButton = (Button) findViewById(R.id.load_button);
+
+        getCityName = (TextView) findViewById(R.id.cityName);
+
+        locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+        locListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                lat = (location.getLatitude());
+                lon = (location.getLongitude());
+
+                List<Address> addresses = null;
+
+                try {
+                    addresses = geocoder.getFromLocation(lat, lon, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                current_city = addresses.get(0).getLocality();
+                if(switcher){
+                    taskLoadUp(current_city);
+                    switcher = false;
+                }
+                //cityField.setText(current_city);
+                //taskLoadUp(current_city);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        configure_button();
+        locButton.performClick();
+        //tutaj
+
+
+
+        //tutaj
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                save_city_name = cityField.getText().toString();
+            }
+        });
+
+
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                taskLoadUp(save_city_name);
+            }
+        });
+        //tutaj
+
+
+
+        //taskLoadUp(save_city_name);
+
 
         selectCity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,6 +210,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //tutaj
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void configure_button(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        locButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //noinspection MissingPermission
+                switcher = true;
+                locManager.requestLocationUpdates("gps", 5000, 0, locListener);
+            }
+        });
+    }
+    //tutaj
+
+
+
     public void taskLoadUp(String query) {
         if (Function.isNetworkAvailable(getApplicationContext())) {
             DownloadWeather task = new DownloadWeather();
@@ -113,7 +265,7 @@ public class MainActivity extends AppCompatActivity {
         }
         protected String doInBackground(String...args) {
             String xml = Function.executeGet("http://api.openweathermap.org/data/2.5/weather?q=" + args[0] +
-                    "&lang=pl&units=metric&appid=" + OPEN_WEATHER_MAP_API);
+                    "&units=metric&appid=" + OPEN_WEATHER_MAP_API);
             return xml;
         }
         @Override
